6cf30ea70b31b6bee8f4895b4e401df971f479cf
Mark J. Becker <herzog31@users.noreply.github.com>
2022-12-07 15:26:38 +0100

    Add footer, copyright, newsletter and footer-links blocks (#25)
    * Add footer, copyright, newsletter and footer-links blocks
    
    * Add back to top button
    
    * Fix linting
    
    * Add feedback
    
    * Skip test

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,19 +1,42 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateMain } from '../../scripts/scripts.js';
+import { loadBlocks } from '../../scripts/lib-franklin.js';
 
-/**
- * loads and decorates the footer
- * @param {Element} block The header block element
- */
+async function loadFragment(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+      decorateMain(main);
+      await loadBlocks(main);
+      return main;
+    }
+  }
+  return null;
+}
 
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
+  const fragment = await loadFragment('/footer');
+  const footer = block.closest('.footer-wrapper');
 
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`);
-  const html = await resp.text();
-  const footer = document.createElement('div');
-  footer.innerHTML = html;
-  await decorateIcons(footer);
-  block.append(footer);
+  if (fragment) {
+    const fragmentSections = fragment.querySelectorAll(':scope .section');
+    if (fragmentSections) {
+      footer.replaceChildren(...fragmentSections);
+    }
+  }
+
+  // Add back to top button
+  const backToTop = document.createRange().createContextualFragment(`
+  <div class="back-to-top">
+    <button aria-label="Back To Top"></button>
+  </div>
+  `);
+
+  // Scroll back to top
+  backToTop.querySelector('button').addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  footer.prepend(backToTop);
 }
