8eb94e92b82da38a2bcec81561ca933d5996f28a
Kiran Murugulla <murugull@adobe.com>
2022-12-09 17:09:48 -0500

    styles pages mentioned in issue-5

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,22 @@
+function transformButtonstoLinks(block) {
+  const buttoncontainers = block.querySelectorAll(':scope .button-container');
+  if (buttoncontainers) {
+    buttoncontainers.forEach((el) => {
+      el.removeAttribute('class');
+      const links = el.querySelectorAll(':scope a.button');
+      if (links) {
+        links.forEach((link) => {
+          link.removeAttribute('class');
+        });
+      }
+    });
+  }
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  if (block.matches('.icons-and-links')) {
+    transformButtonstoLinks(block);
+  }
 }
