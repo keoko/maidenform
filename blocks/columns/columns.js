c0d8d1a9d68fd16347f4d8b95c29883e2f1c81ee
Kiran Murugulla <murugull@adobe.com>
2022-12-16 04:39:27 -0500

    Issue 49 (#53)
    * moving decorateButtons() to scripts.js and keeping only primary and secondary buttons
    
    - Requires customer-service columns block or any other areas styled links to be refactored
    
    * removing functions no longer required after decorateButton() changes
    
    * changes to improve CLS score for customer service page

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,19 +1,3 @@
-function transformButtonstoLinks(block) {
-  const buttoncontainers = block.querySelectorAll(':scope .button-container');
-  // Removing 'button-container and 'button' class from 'p' and 'a' elements in this block
-  if (buttoncontainers) {
-    buttoncontainers.forEach((el) => {
-      el.removeAttribute('class');
-      const links = el.querySelectorAll(':scope a.button');
-      if (links) {
-        links.forEach((link) => {
-          link.removeAttribute('class');
-        });
-      }
-    });
-  }
-}
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -24,9 +8,5 @@ export default function decorate(block) {
       // Take second column div and move into first column
       cols[0].appendChild(cols[1]);
     }
-  }
-  // Handle button to link transformation
-  if (block.matches('.icons-and-links')) {
-    transformButtonstoLinks(block);
   }
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,6 @@ import {
   sampleRUM,
   loadHeader,
   loadFooter,
-  decorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
@@ -25,6 +24,32 @@ window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information 
     main.prepend(section);
   }
 } */
+/**
+ * decorates paragraphs containing a single link as buttons.
+ * @param {Element} element container element
+ */
+
+function decorateButtons(element) {
+  element.querySelectorAll('a').forEach((a) => {
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
+          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
+          a.className = 'button primary';
+          twoup.classList.add('button-container');
+        }
+        if (up.childNodes.length === 1 && up.tagName === 'EM'
+          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
+          a.className = 'button secondary';
+          twoup.classList.add('button-container');
+        }
+      }
+    }
+  });
+}
 
 /**
  * Builds all synthetic blocks in a container element.
