c370036cf371f1b6737b047bc016b320a7e6bd86
Mark J. Becker <herzog31@users.noreply.github.com>
2022-12-21 19:39:44 +0100

    Rename columns teaser style to overlap (#60)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
 
   // Handling of teaser style
-  if (block.classList.contains('teaser')) {
+  if (block.classList.contains('overlap')) {
     if (cols.length > 1) {
       // Take second column div and move into first column
       cols[0].appendChild(cols[1]);
