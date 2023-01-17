609bbfc66432d79f618b0993966f100b2ec2d55d
Hannes Hertach <hhertach@adobe.com>
2023-01-17 10:23:02 +0100

    Pdp preact (#91)
    * port pdp carousel logic to preact
    
    * reimplement pdp in preact
    
    * fix icons
    
    * add dynamic image sizing
    
    * fix lint error
    
    * add preact source files
    
    * fix thumbnail scrolling in pdp carousel

---
+++ blocks/product-details/ProductDetailsCarousel.js
@@ -0,0 +1,106 @@
+import {
+  h, Component, Fragment,
+} from '../../scripts/preact.js';
+import htm from '../../scripts/htm.js';
+import Icon from './Icon.js';
+
+const html = htm.bind(h);
+
+function OptimizedSources({
+  src,
+  sizes,
+  width,
+  height,
+}) {
+  return html`
+    <${Fragment} >
+        ${sizes.map((size) => {
+    const url = new URL(src);
+    url.searchParams.set('width', size.width);
+    return html`
+        <source media=${`(max-width: ${size.media}px)`} srcset=${url.href}/>`;
+  })
+}
+        <img height=${height} width=${width} src=${src} />
+    </Fragment>
+  `;
+}
+
+export default class Carousel extends Component {
+  constructor() {
+    super();
+    this.state = {
+      slide: 0,
+      thumbnailSlide: 0,
+    };
+
+    this.thumbnailImages = [
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Front.jpg?width=247&quality=100&bg-color=255,255,255',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Side.jpg?width=247&quality=100&bg-color=255,255,255',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Back.jpg?width=247&quality=100&bg-color=255,255,255',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Detail01.jpg?width=247&quality=100&bg-color=255,255,255',
+    ];
+
+    this.images = [
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Front.jpg?width=700&quality=100&bg-color=255,255,255&dpr=1 1x',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Side.jpg?width=700&quality=100&bg-color=255,255,255&dpr=1 1x',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Back.jpg?width=700&quality=100&bg-color=255,255,255&dpr=1 1x',
+      'https://cdn.maidenform.com/catalog/product/M/F/MFB_09436/MFB_09436_Black_Detail01.jpg?width=700&quality=100&bg-color=255,255,255&dpr=1 1x',
+    ];
+  }
+
+  static negativeModulo(i, mod) {
+    return ((i % mod) + mod) % mod;
+  }
+
+  updateThumbnailSlide(getNextIndex) {
+    const nextIndex = getNextIndex(this.state.thumbnailSlide);
+    const limitedIndex = Math.min(Math.max(nextIndex, 0), this.thumbnailImages.length - 1);
+    this.setState({ thumbnailSlide: limitedIndex });
+  }
+
+  updateSlide(getNextIndex) {
+    const nextIndex = getNextIndex(this.state.slide);
+    const correctedNextIndex = Carousel.negativeModulo(nextIndex, this.images.length);
+    this.setState({
+      slide: correctedNextIndex,
+      thumbnailSlide: correctedNextIndex,
+    });
+  }
+
+  render() {
+    return html`
+        <div class="product-detail-carousel">
+            <div class="carousel-thumbnails-wrapper">
+                <div class="thumbnail-controls">
+                    <button name="thumbnail-prev" onClick=${() => this.updateThumbnailSlide((index) => index - 1)}><${Icon} name="caret-up-fill" /></button>
+                    <button name="thumbnail-next" onClick=${() => this.updateThumbnailSlide((index) => index + 1)}><${Icon} name="caret-down-fill" /></button>
+                </div>
+                <ul class="carousel-thumbnails" style="transform: translateY(-${(this.state.thumbnailSlide === 0 ? 0 : 1) * -252.7 + (this.state.thumbnailSlide) * 322.6}px)">
+                    ${this.thumbnailImages.map((image, i) => html`
+                          <li key=${image} onClick=${() => this.setState({ slide: i, thumbnailSlide: i })}>
+                              <picture>
+                                  <img height="313" width="247" src=${image} />
+                              </picture>
+                          </li>`)}
+                </ul>
+            </div>
+            <div class="carousel-stage-wrapper">
+                <div class="main-controls">
+                    <button name="stage-prev" onClick=${() => this.updateSlide((index) => index - 1)}><${Icon} name="caret-left-fill" /></button>
+                    <button name="stage-next" onClick=${() => this.updateSlide((index) => index + 1)}><${Icon} name="caret-right-fill" /></button>
+                </div>
+                <ul class="carousel-stage" style="transform: translateX(-${this.state.slide * 100}%)">
+                    ${this.images.map((image, i) => html`
+                        <li key=${image} active=${i === this.state.slide ? 'true' : 'false'}>
+                            <picture>
+                                <${OptimizedSources} src=${image} width="888" height="700" sizes=${[{ media: 450, width: 450 }, { media: 2000, width: 700 }]} />
+                            </picture>
+                        </li>
+                    `)}
+                </ul>
+            </div>
+        </div>
+    `;
+  }
+}
---
+++ blocks/product-details/ProductDetailsSidebar.js
@@ -0,0 +1,190 @@
+import {
+  h, Component, Fragment,
+} from '../../scripts/preact.js';
+import htm from '../../scripts/htm.js';
+import Icon from './Icon.js';
+
+const html = htm.bind(h);
+
+function SizeSelector({
+  sizeType, allSizes, unavailableSizes, onChange, selectedSize,
+}) {
+  return html`
+      <div class="sidebar-section sizes-selector">
+          <h4>AVAILABLE ${sizeType.toUpperCase()}</h4>
+          <span class="size-guide">Size Guide</span>
+          <ul>
+              ${allSizes.map((size) => html`
+                <li aria-selected=${size === selectedSize} 
+                    onClick=${unavailableSizes?.includes(size) || (() => onChange?.(size))} 
+                    key=${size} 
+                    aria-disabled=${unavailableSizes?.includes(size)}>
+                    ${size}
+                </li>
+              `)}
+          </ul>
+      </div>
+  `;
+}
+
+function QuantitySelector({ onChange }) {
+  return html`
+      <div class="sidebar-section quantity-select">
+          <h4 class="selection">QUANTITY:</h4>
+          <select onChange=${(option) => onChange?.(option.value)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+          </select>
+      </div>
+  `;
+}
+
+function NameAndPrice({ name, price }) {
+  return html`
+    <${Fragment}>
+        <h1>${name}</h1>
+        <div class="price">
+            <span class="price-reduced">${price?.actual}</span>
+            <span class="price-actual">${price?.reduced}</span>
+        </div>
+        <div class="style-id">Style #09436</div>
+    </Fragment>
+  `;
+}
+
+function Rating({ value }) {
+  return html`
+      <div class="product-rating">
+          ${[...Array(5).keys()].map((key) => {
+    if (value - key < 0.5) return html`<${Icon} name="star" />`;
+    if (value - key > 0.5) return html`<${Icon} name="star-fill" />`;
+    return html`<${Icon} name="star-half" />`;
+  })}
+      </div>
+  `;
+}
+
+function ColorSelector({ colors, onChange, selectedColor }) {
+  return html`
+      <div class="sidebar-section color-selector">
+          <h4>AVAILABLE COLOR</h4>
+          <ul class="swatches">
+              ${colors.map((color) => html`
+                      <li 
+                        onClick=${() => onChange?.(color.name)} 
+                        aria-selected=${color.name === selectedColor} 
+                        key=${color.name} data-swatch-name=${color.name} 
+                        class="swatch" 
+                        style="background: url(${color.url}) no-repeat center;"></li>
+                  `)}
+          </ul>
+          <div class="selected-swatch-name">${selectedColor}</div>
+      </div>
+  `;
+}
+
+function CartSection() {
+  return html`<${Fragment}>
+      <div class="sidebar-section cart">
+          <button class="button primary cart-button">Add to Bag</button>
+          <p>Pay in 4 interest free payments on purchases of $30-$1,500 with PayPal
+          </p>
+          <p class="secondary-action"><${Icon} name="heart" />ADD TO FAVORITES</p>
+          <p class="secondary-action"><${Icon} name="envelope" />EMAIL</p>
+          <p><a href="/customer-service">Need Help?</a></p>
+      </div>
+  </>
+  `;
+}
+
+function SelectionDisplay({ selection }) {
+  return html`
+      <div class="sidebar-section">
+          <h4 class="variant-selection">
+              <span>SELECTION: </span>
+              <span>
+                    ${selection.color} ${selection.bandSize}${selection.cupSize} 
+                  </span>
+          </h4>
+      </div>
+  `;
+}
+
+export default class ProductDetailsSidebar extends Component {
+  constructor() {
+    super();
+
+    // Subject to change once commerce endpoint is available
+    const product = {
+      name: 'Everyday Full Coverage Cushioned Underwire Bra',
+      price: {
+        actual: '$48.00',
+        reduced: '$28.80',
+      },
+      rating: 3.5,
+      colors: [
+        { name: 'Black', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_Black_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'White', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_White_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Black and Navy Blossoms Print', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_BlackandNavyBlossomsPrint_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Sandshell', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_Sandshell_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Body Beige', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_BodyBeige_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Ivory', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_Ivory_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Bleached Indigo', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_BleachedIndigo_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Chestnut', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_Chestnut_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+        { name: 'Lilac Meringue', url: 'https://swatches.maidenform.com/HNS_09436/HNS_09436_LilacMeringue_sw.jpg?quality=85&height=50&width=50&fit=bounds' },
+      ],
+    };
+
+    this.state = {
+      selection: {
+        color: 'Black',
+        cupSize: null,
+        bandSize: null,
+      },
+      product,
+    };
+  }
+
+  updateSelection(fragment) {
+    this.setState((state) => ({ selection: { ...state.selection, ...fragment } }));
+  }
+
+  render() {
+    return html`<${Fragment}>
+      <div class="product-title desktop-hidden">
+          <${NameAndPrice} name=${this.state.product.name} price=${this.state.product.price} />
+      </div>
+      <div class="sidebar">
+          <div class="product-title sidebar-section mobile-hidden">
+              <${Rating} value=${this.state.product.rating} />
+              <${NameAndPrice} name=${this.state.product.name} price=${this.state.product.price} />
+          </div>
+          <${ColorSelector} 
+                  colors=${this.state.product.colors}
+                  onChange=${(color) => this.updateSelection({ color })}
+                  selectedColor=${this.state.selection.color}
+          />
+          <${SizeSelector} 
+                  sizeType="Band Size" 
+                  allSizes=${[34, 36, 38, 40, 42]} 
+                  unavailableSizes=${[38]} 
+                  selectedSize=${this.state.selection.bandSize}
+                  onChange=${(size) => this.updateSelection({ bandSize: size })}
+          />
+          <${SizeSelector} 
+                  sizeType="Cup Size" 
+                  allSizes=${['B', 'C', 'D', 'DD']} 
+                  unavailableSizes=${['DD']}
+                  selectedSize=${this.state.selection.cupSize}
+                  onChange=${(size) => this.updateSelection({ cupSize: size })}
+          />
+          <${SelectionDisplay} selection=${this.state.selection} />
+          <${QuantitySelector} />
+          <${CartSection} />
+      </div>
+    </Fragment>`;
+  }
+}
