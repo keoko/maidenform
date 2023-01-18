fc057cde18545fa32a2660b16e91a03adeb2b24a
Hannes Hertach <hanneshertach@gmail.com>
2023-01-18 12:18:33 +0100

    add shimmer loading effect for desktop

---
+++ blocks/product-details/ProductDetailsShimmer.js
@@ -0,0 +1,17 @@
+import {
+  h, Fragment
+} from '../../scripts/preact.js';
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+
+export default function ProductDetailsShimmer() {
+  return html`<${Fragment}>
+      <div class="carousel-shimmer">
+          <div class="carousel-thumbnails-shimmer shimmer"></div>
+          <div class="carousel-stage-shimmer shimmer"></div>
+      </div>
+      <div class="sidebar-shimmer shimmer"></div>
+      <div class="detail-shimmer shimmer"></div>
+  </Fragment>`;
+}
