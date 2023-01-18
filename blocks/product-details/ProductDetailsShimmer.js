498455b4a615ec48ede462a8720a3d5fe6be6564
Hannes Hertach <hanneshertach@gmail.com>
2023-01-18 13:47:53 +0100

    add mobile shimmers

--- blocks/product-details/ProductDetailsShimmer.js
+++ blocks/product-details/ProductDetailsShimmer.js
@@ -7,11 +7,15 @@ const html = htm.bind(h);
 
 export default function ProductDetailsShimmer() {
   return html`<${Fragment}>
+      <div class="desktop-hidden title-shimmer">
+          <div class="heading-shimmer shimmer"></div>
+          <div class="price-shimmer shimmer"></div>
+      </div>
       <div class="carousel-shimmer">
           <div class="carousel-thumbnails-shimmer shimmer"></div>
           <div class="carousel-stage-shimmer shimmer"></div>
       </div>
       <div class="sidebar-shimmer shimmer"></div>
-      <div class="detail-shimmer shimmer"></div>
+      <div class="detail-shimmer shimmer mobile-hidden"></div>
   </Fragment>`;
 }
