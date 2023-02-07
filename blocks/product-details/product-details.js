7b25cb0fdd7485897363a98d05ee7222c3f32208
Hannes Hertach <hanneshertach@gmail.com>
2023-02-07 08:34:21 +0100

    add query

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -8,18 +8,124 @@ import ProductDetailsShimmer from './ProductDetailsShimmer.js';
 
 const html = htm.bind(h);
 
+const query = `
+{
+  products(skus:["09436"]) {
+    __typename
+    id
+    sku
+    name
+    description
+    shortDescription
+    url
+    images(roles: []) {
+      url
+      label
+      roles
+    }
+    attributes(roles: []) {
+      name
+      label
+      value
+      roles
+    }
+  ... on SimpleProductView {
+      price {
+        final {
+          amount {
+            value
+            currency
+          }
+        }
+        regular {
+          amount {
+            value
+            currency
+          }
+        }
+      }
+    }
+  ... on ComplexProductView {
+      options {
+        id
+        title
+        required
+        values {
+          id
+          title
+  
+        }
+      }
+      priceRange {
+        maximum {
+          final {
+            amount {
+              value
+              currency
+            }
+          }
+          regular {
+            amount {
+              value
+              currency
+            }
+          }
+        }
+        minimum {
+          final {
+            amount {
+              value
+              currency
+            }
+          }
+          regular {
+            amount {
+              value
+              currency
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
 class ProductDetailPage extends Component {
   constructor() {
     super();
-    this.state = { loading: false };
+    this.state = { loading: true };
+  }
+
+  async loadData() {
+    // TODO start data loading before loading preact if possible
+    const headers = {
+      'Content-Type': 'application/json',
+      'Magento-Environment-Id': '271c8746-f2ed-43c3-8159-e7b7bbe79aac',
+      'Magento-Website-Code': 'maidenform',
+      'Magento-Store-View-Code': 'maidenform_store_view',
+      'Magento-Store-Code': 'maidenform_store',
+      'Magento-Customer-Group': '77de68daecd823babbb58edb1c8e14d7106e83bb',
+      'x-api-key': '070cb248e40b4f20b72836d1cd1dbbaf',
+    };
+
+    const response = await fetch('https://catalog-service-sandbox.adobe.io/graphql', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ query }),
+    });
+
+    if (response.ok) {
+      const productData = await response.json();
+      this.setState({ productData });
+      console.log(productData)
+    }
+    // TODO error handling
+    this.setState({ loading: false });
   }
 
   componentDidMount() {
-    if (this.state.loading) {
-      setTimeout(() => {
-        this.setState({ loading: false });
-      }, 1600);
-    }
+    this.loadData();
   }
 
   render() {
