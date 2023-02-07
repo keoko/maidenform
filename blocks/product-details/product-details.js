760465783348be1ba5eaeb6c0a611534c8d0b59b
Hannes Hertach <hanneshertach@gmail.com>
2023-02-07 11:55:10 +0100

    add optimized loading

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -1,5 +1,5 @@
 import {
-  h, render, Fragment, Component,
+  Component, Fragment, h, render,
 } from '../../scripts/preact.js';
 import htm from '../../scripts/htm.js';
 import Carousel from './ProductDetailsCarousel.js';
@@ -91,44 +91,57 @@ const query = `
 }
 `;
 
+async function performRequest() {
+  // TODO start data loading before loading preact if possible
+  const headers = {
+    'Content-Type': 'application/json',
+    'Magento-Environment-Id': '271c8746-f2ed-43c3-8159-e7b7bbe79aac',
+    'Magento-Website-Code': 'maidenform',
+    'Magento-Store-View-Code': 'maidenform_store_view',
+    'Magento-Store-Code': 'maidenform_store',
+    'Magento-Customer-Group': '77de68daecd823babbb58edb1c8e14d7106e83bb',
+    'x-api-key': '070cb248e40b4f20b72836d1cd1dbbaf',
+  };
+
+  const response = await fetch('https://catalog-service-sandbox.adobe.io/graphql', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ query }),
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+  return null;
+}
+
 class ProductDetailPage extends Component {
-  constructor() {
+  constructor(props) {
     super();
-    this.state = { loading: true };
+
+    if (props.data.needAwait) {
+      this.state = { loading: true, promise: props.data.promise };
+    } else {
+      this.state = { loading: false, data: props.data.data };
+    }
   }
 
   async loadData() {
-    // TODO start data loading before loading preact if possible
-    const headers = {
-      'Content-Type': 'application/json',
-      'Magento-Environment-Id': '271c8746-f2ed-43c3-8159-e7b7bbe79aac',
-      'Magento-Website-Code': 'maidenform',
-      'Magento-Store-View-Code': 'maidenform_store_view',
-      'Magento-Store-Code': 'maidenform_store',
-      'Magento-Customer-Group': '77de68daecd823babbb58edb1c8e14d7106e83bb',
-      'x-api-key': '070cb248e40b4f20b72836d1cd1dbbaf',
-    };
-
-    const response = await fetch('https://catalog-service-sandbox.adobe.io/graphql', {
-      method: 'POST',
-      headers,
-      body: JSON.stringify({ query }),
-    });
-
-    if (response.ok) {
-      const productData = await response.json();
-      this.setState({ productData });
-      console.log(productData)
-    }
     // TODO error handling
     this.setState({ loading: false });
   }
 
   componentDidMount() {
-    this.loadData();
+    if (this.state.promise) {
+      this.state.promise.then((data) => {
+        this.setState({ loading: false, data });
+      });
+    }
   }
 
   render() {
+    console.log(this.state.data)
+
     if (this.state.loading) {
       return html`<${ProductDetailsShimmer} />`;
     }
@@ -148,9 +161,26 @@ class ProductDetailPage extends Component {
   }
 }
 
-export default function decorate($block) {
+async function dataOrLoading(timeout) {
+  const dataPromise = performRequest();
+
+  return Promise.any([
+    new Promise((res) => {
+      dataPromise.then((data) => res({ needAwait: false, data }));
+    }),
+    new Promise((res) => {
+      setTimeout(() => res({ needAwait: true, promise: dataPromise }), timeout);
+    }),
+  ]);
+}
+
+export default async function decorate($block) {
   $block.innerHTML = '';
-  const app = html`<${ProductDetailPage} />`;
+
+  // if data is loaded within 50ms, don't show loading effect
+  const result = await dataOrLoading(50);
+
+  const app = html`<${ProductDetailPage} data=${result} />`;
 
   render(app, $block);
 }
