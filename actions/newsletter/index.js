88593309f752df2d6d2171bb8b6b42554fbf348c
Hannes Hertach <hanneshertach@gmail.com>
2023-03-16 19:23:40 +0100

    add code to submit newsletter

--- actions/newsletter/index.js
+++ actions/newsletter/index.js
@@ -7,18 +7,53 @@ async function main(params) {
     return { statusCode: 400 };
   }
 
-  const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+  // Validate Captcha
+  const captchaResponse = await fetch('https://www.google.com/recaptcha/api/siteverify', {
     method: 'POST',
     body: new URLSearchParams({ secret: params.CAPTCHA_SECRET_KEY, response: captchaToken }),
   });
-  const data = await response.json();
+  const data = await captchaResponse.json();
 
-  if (!response.ok || !data.success) {
+  if (!captchaResponse.ok || !data.success) {
     console.log(data);
-    return { statusCode: 500 };
+    return { statusCode: 400, body: 'Captcha Invalid' };
   }
 
-  return { statusCode: 200 };
+  // Request sign up to newsletter
+  const siteId = params.ZETA_SITE_ID;
+  const password = params.ZETA_PASSWORD;
+  const newsletterResponse = await fetch(`https://api.zetaglobal.net/ver2/${siteId}/activities`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${Buffer.from(`api:${password}`).toString('base64')}`,
+    },
+    body: JSON.stringify({
+      activity: {
+        subscriber: {
+          uid: email,
+        },
+        event: 'email_registration',
+        properties: {
+          esite: 'mfb',
+          registration_source: 'sitefooter',
+        },
+      },
+    }),
+  });
+
+  if (!newsletterResponse.ok) {
+    const responseStatus = newsletterResponse.status;
+    const responseBody = await newsletterResponse.json();
+    console.log(responseStatus, responseBody);
+    return {
+      statusCode: 500,
+      body: 'Error from Newsletter API',
+    };
+  }
+
+  return { statusCode: 200, body: 'Success' };
 }
 
 exports.main = main;
