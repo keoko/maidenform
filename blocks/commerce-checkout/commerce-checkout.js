6b76dd113050fad78e6a9613536aef724f15c0da
Felix Delval <fdelval@adobe.com>
2023-03-27 15:45:04 +0200

    Building latest and it working OK

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -4,7 +4,7 @@ import {
   import htm from '../../scripts/htm.js';
 import { Checkout } from "../../scripts/commerce-checkout/containers/Checkout.js";
 import { renderer } from "../../scripts/commerce-checkout/renderer.js";
-import { setSelectedStore } from '../../scripts/commerce-checkout/cart/api.js';
+import { setSelectedStore } from '../../scripts/commerce-checkout/api.js';
 
 const html = htm.bind(h);
 
@@ -12,11 +12,11 @@ export default async function decorate(block) {
     block.innerHTML = '<div class="full-height"></div>';
 
     const render = renderer({
-        endpoint: 'https://graph.adobe.io/api/63e62e43-8eb8-45a2-b0f6-f7c3845093db/graphql?api_key=2c6d06bb3aef463db8485c88a90f563f',
-        mesh: 'storefrontstaticenvmesh',
+        endpoint: 'https://franklin.maidenform.com/graphql',
       });
 
-    setSelectedStore('default')
+      setSelectedStore('maidenform_store_view')
+
 
     const checkout = document.createElement("div")
   
--- scripts/commerce-checkout/mf-239.js
+++ scripts/commerce-checkout/mf-239.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[239],{3239:(e,k,h)=>{h.r(k),h.d(k,{Checkout:()=>u.h,CheckoutSkeleton:()=>c.I});var u=h(4975),c=h(4459)}}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[239],{3239:(e,k,h)=>{h.r(k),h.d(k,{Checkout:()=>u.h,CheckoutSkeleton:()=>c.I});var u=h(13),c=h(4459)}}]);
