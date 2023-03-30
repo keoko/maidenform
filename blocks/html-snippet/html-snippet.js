672df739bf09aaa7b6fe0f8d7602d1da13a90506
Alberto Di Cagno <adicagno@adobe.com>
2023-03-30 16:05:19 +0200

    feat: scheduler for html snippet rendering

--- blocks/html-snippet/html-snippet.js
+++ blocks/html-snippet/html-snippet.js
@@ -1,4 +1,37 @@
 import { getFragmentFromFile } from '../../scripts/scripts.js';
+import '../../scripts/dayjs.min.js';
+
+const { dayjs } = window;
+
+const parseDates = (rawDates) => {
+  let {
+    from,
+    to,
+  } = { ...rawDates };
+
+  if (from) {
+    from = dayjs(from);
+    if (!from.isValid()) {
+      throw new Error('dateFrom is not valid');
+    }
+  }
+
+  if (to) {
+    to = dayjs(to);
+    if (!to.isValid()) {
+      throw new Error('dateTo is not valid');
+    }
+  }
+
+  if (from && to && (to.isBefore(from) || to.isSame(from))) {
+    throw new Error('dateFrom must be before (and different from) dateTo');
+  }
+
+  return {
+    from,
+    to,
+  };
+};
 
 function loadScript(parentElem, originalElem) {
   const script = document.createElement('script');
@@ -30,29 +63,72 @@ function processScriptNodes(parent, node) {
   return node;
 }
 
-const getSnippetURLs = (children) => children.map((child) => {
-  const u = child.children[0].textContent;
-  try {
-    const url = new URL(u);
-    // only allow https
-    if (url.protocol !== 'https:') {
-      url.protocol = 'https:';
-    }
-    return url.href;
-  } catch (e) { // fallback in case of malformed URL
-    return u;
-  }
-});
+const checkRawDate = (rawDate) => ((rawDate === 'inf' || rawDate === '0' || rawDate === 'infinity' || rawDate === '') ? undefined : rawDate);
+
+const getSnippetParams = (children) => {
+  const dateRange = children[children.length - 1].textContent
+    .toLowerCase()
+    .split('-')
+    .map((d) => d.trim());
+
+  const [dateFrom, dateTo] = dateRange;
+
+  return {
+    urls: (children.length > 1 ? children.slice(0, -1) : children).map((child) => {
+      const u = child.children[0].textContent;
+      try {
+        const url = new URL(u);
+        // only allow https
+        if (url.protocol !== 'https:') {
+          url.protocol = 'https:';
+        }
+        return url.href;
+      } catch (e) { // fallback in case of malformed URL
+        return u;
+      }
+    }),
+    rawDates: children.length > 1
+    && children[children.length - 1] ? {
+        from: checkRawDate(dateFrom),
+        to: checkRawDate(dateTo),
+      } : undefined,
+    scheduled: !!(dateFrom || dateTo),
+  };
+};
+
+async function decorateSnippet(block, fragmentURL) {
+  const fragment = await getFragmentFromFile(fragmentURL);
+  block.innerHTML = fragment.innerHTML;
+  processScriptNodes(block, fragment);
+}
+
+function isSnippetScheduledNow(interval) {
+  const now = dayjs();
+
+  return (!interval.from || (now.isSame(interval.from) || now.isAfter(interval.from)))
+    && (!interval.to || ((now.isSame(interval.to) || now.isBefore(interval.to))));
+}
+
 export default async function decorate(block) {
   const children = [...block.children];
-  const urls = getSnippetURLs(children);
-  const fragmentURL = urls[0]; // rendering one fragment only as per current requirements
+
+  const {
+    urls,
+    rawDates,
+    scheduled,
+  } = getSnippetParams(children);
+  const url = urls[0];// rendering one fragment only as per current requirements
+
   try {
-    const fragment = await getFragmentFromFile(fragmentURL);
-    block.innerHTML = fragment.innerHTML;
-    processScriptNodes(block, fragment);
+    const interval = parseDates(rawDates);
+
+    if (!scheduled || isSnippetScheduledNow(interval)) {
+      await decorateSnippet(block, url, interval);
+      return;
+    }
   } catch (e) {
     block.textContent = '';
-    console.warn(`cannot load snippet at ${fragmentURL}: ${e}`);
+    console.warn(`cannot load snippet at ${url}: ${e}`);
   }
+  block.textContent = '';
 }
